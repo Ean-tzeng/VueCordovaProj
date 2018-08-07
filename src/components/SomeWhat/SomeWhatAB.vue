29095ae634c6fa37634fef854f3be3d25a5ebc95
Ean Tzeng (曾文彥) <ean.tzeng@eih.eink.com>
2018-08-07 10:17:07 +0800

    加入BARCODE功能

--- src/components/SomeWhat/SomeWhatAB.vue
+++ src/components/SomeWhat/SomeWhatAB.vue
@@ -1,7 +1,48 @@
 <template>
     <v-ons-page>
-      AB
+      <button v-on:click="test">test</button>
+      <input type="text" v-model="data">
     </v-ons-page>
 </template>
+<script>
+import vue from 'vue'
+export default {
+  data(){
+    return {
+      data:'test'
+    }
+  },
+  methods: {
+    test(){
+      var vueobj=this
+      cordova.plugins.barcodeScanner.scan(
+        function (result) {
+            alert("We got a barcode\n" +
+                  "Result: " + result.text + "\n" +
+                  "Format: " + result.format + "\n" +
+                  "Cancelled: " + result.cancelled)
+            vueobj.data= result.text
+        },
+        function (error) {
+            alert("Scanning failed: " + error);
+        },
+        {
+            preferFrontCamera : false, // iOS and Android
+            showFlipCameraButton : true, // iOS and Android
+            showTorchButton : true, // iOS and Android
+            torchOn: true, // Android, launch with the torch switched on (if available)
+            saveHistory: true, // Android, save scan history (default false)
+            prompt : "Place a barcode inside the scan area", // Android
+            resultDisplayDuration: 500, // Android, display scanned text for X ms. 0 suppresses it entirely, default 1500
+            formats : "QR_CODE,PDF_417,CODE_128,CODE_39", // default: all but PDF_417 and RSS_EXPANDED
+            orientation : "landscape", // Android only (portrait|landscape), default unset so it rotates with the device
+            disableAnimations : true, // iOS
+            disableSuccessBeep: false // iOS and Android
+        }
+      );
+    }
+  }
+}
+</script>
 
 
